d7d6c261a6444e0f5a218c43d48022b6179c3c07
Egor <egot.dubovczcev@yandex.ru>
2020-04-01 08:45:22 +0400

    delete all for pull request

--- Main.java
+++
@@ -1,9 +0,0 @@
-import service.GameService;
-
-public class Main {
-
-    public static void main(String[] args) {
-        GameService gameService = new GameService();
-        gameService.startGame();
-    }
-}
